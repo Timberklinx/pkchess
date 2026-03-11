9d1001c7a925d9ef27e3bb8edc1a0c617efc357a
Timberklinx <yoann_ardhuin@outlook.com>
2026-03-11 10:51:27 +0100

    fix static folder

--- pkchess-app/main.py
+++ pkchess-app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
 import json, random, string, asyncio
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 # ── État des parties en mémoire ──────────────────────────────────────────────
